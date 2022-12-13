352490d2ef9305ddb4b6674036b4e556c271c845
Guy B <91497242+guyb2022@users.noreply.github.com>
2022-12-13 09:53:33 +0200

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@
 
 # [START video_detect_faces_gcs]
 import time
-from google.cloud import videointelligence_v1 as videointelligence
+from google.cloud import videointelligence
 
 gcs_uri = "gs://my_first_bucket_vac/videoplayback.mp4"
 output_uri = "gs://result_ojt_after_api/output - {}.json".format(time.time())
